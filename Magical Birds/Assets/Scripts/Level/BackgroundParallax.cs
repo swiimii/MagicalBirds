9a27b42d0d552733e42203fc114f3e9aa71de611
Samuel Scherer <scheresr@mail.uc.edu>
2019-07-02 18:47:29 -0400

    Made several organization changes, added backgrnd
    Placed scripts within a folder organization area, added a seven-layer background to which I plan to add a parallax effect (as well as some parallax scripts), and added a references document within a "documents" folder

---
+++ Magical Birds/Assets/Scripts/Level/BackgroundParallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    // I am making this script for specific use with Foxly's 7-layer pixel art background
+    // is made in tandem with BackgroundPieceController
+    public BackgroundPieceController background1;
+    public BackgroundPieceController background2;
+
+    // Move the background left as the player moves right
+    public void ScrollLeft()
+    {
+
+    }
+
+    // Move the background right as the player moves left
+    public void ScrollRight()
+    {
+
+    }
+
+}
---
+++ Magical Birds/Assets/Scripts/Level/BackgroundPieceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundPieceController : MonoBehaviour
+{
+    // This script simply keeps track of a background GameObjects layers.
+    // For use with BackgroundParallax.cs
+    public GameObject[] layers;
+    public float[] parallaxChangeValueX;
+    public float[] parallaxChangeValueY;
+
+}
