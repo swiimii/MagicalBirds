121628708f1b9ae3d2f37cb1159ea336b8ea1b72
Cam Skubik-Peplaski <skubikcj@mail.uc.edu>
2019-07-09 19:43:05 -0400

    Added interaction system; WIP game state, currently non-functional

---
+++ Magical Birds/Assets/Scripts/CharacterScripts/Player/PlayerInteract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInterItem = null;
+
+    void Update() {
+        if(Input.GetButtonDown("Interact") && currentInterItem != null) {
+            currentInterItem.SendMessage("DoInteract");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Item")) {
+            currentInterItem = other.gameObject;
+        } else if(other.CompareTag("Item")) {
+            //TODO: Pickup items logic
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Item")) {
+            if(other.gameObject == currentInterItem){
+                currentInterItem = null;
+            }
+        }
+    }
+}
---
+++ Magical Birds/Assets/Scripts/Game/StateManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager
+{
+    private static StateManager instance;
+    // Active Level
+    private int activeLevel;
+    // Maximum HP
+    private int maxHP;
+    // Current HP
+    private int hp;
+    // Damage Player Does
+    private int dmg;
+    // Items Collected
+    private string[] collectedItems;
+    // # of items collected, used to index
+    private int itemsCollected;
+
+    public static StateManager Instance{
+        get{
+            if(instance == null){
+                instance = new StateManager();
+            }
+
+            return instance;
+        }
+    }
+
+    // TODO: Remove later when there's something else to start it
+    // Start is called before the first frame update
+    void Start()
+    {
+        startState();
+    }
+
+    private void OnApplicationQuit() {
+        instance = null;
+    }
+
+    public void startState(){
+        Debug.Log("Creating new game state manager");
+
+        //Set defaults
+        activeLevel = 1;
+        maxHP = 6;
+        hp = 6;
+        dmg = 2;
+        itemsCollected = 0;
+        collectedItems = new string[3];
+    }
+
+    public void addCollectedItem(string collectedItem){
+        collectedItems[itemsCollected] = collectedItem;
+        itemsCollected++;
+        Debug.Log("Added " + collectedItem + " to items collected");
+    }
+}
