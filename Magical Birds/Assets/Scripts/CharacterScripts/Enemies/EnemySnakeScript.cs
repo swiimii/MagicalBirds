6992e32ae1c5c2c22bd202b839e8bbcc52f537ab
Samuel Scherer <scheresr@mail.uc.edu>
2019-07-09 13:54:37 -0400

    Started working on Snake enemy

---
+++ Magical Birds/Assets/Scripts/CharacterScripts/Enemies/EnemySnakeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySnakeScript : MonoBehaviour
+{
+    public int damageDealt = 1;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerResourcesController>().Damage(damageDealt, transform.position);//, transform.position);
+        }
+    }
+
+}
