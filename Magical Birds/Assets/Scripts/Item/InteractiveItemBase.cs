795b42c9b94995d18d3446c5cee39a46f20c06ee
Cam Skubik-Peplaski <skubikcj@mail.uc.edu>
2019-07-09 19:43:05 -0400

    Added interaction system; WIP game state, currently non-functional

---
+++ Magical Birds/Assets/Scripts/Item/InteractiveItemBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveItemBase : MonoBehaviour
+{
+    public string itemName;
+    public Sprite image;
+    public virtual void DoInteract(){
+        gameObject.SetActive(false);
+        StateManager.Instance.addCollectedItem(itemName);
+    }
+}
