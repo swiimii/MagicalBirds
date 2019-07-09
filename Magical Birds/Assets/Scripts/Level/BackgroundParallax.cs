ae278ce64492bb8c0d951045376f301123f2ee89
Samuel Scherer <scheresr@mail.uc.edu>
2019-07-09 13:12:26 -0400

    Created 7-layer Parallax
    There are some issues with the parallax - notably, there is a small gap between layers when a layer is moved right / left. This isn't a huge problem, and can be fixed later.

--- Magical Birds/Assets/Scripts/Level/BackgroundParallax.cs
+++ Magical Birds/Assets/Scripts/Level/BackgroundParallax.cs
@@ -1,44 +1,104 @@
-﻿using System.Collections;
+﻿// Script made by Samuel Scherer
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundParallax : MonoBehaviour
 {
     // I am making this script for specific use with Foxly's 7-layer pixel art background
-    // is made in tandem with BackgroundPieceController
-    public BackgroundPieceController background1;
-    public BackgroundPieceController background2;
-    public float verticalScrollCoefficient;
-    public float horizontalScrollCoefficient;
+    // is made in tandem with BackgroundPieceState.cs
+    public BackgroundPieceState background1; 
+    public BackgroundPieceState background2;
 
-    // Only move the screen within a certain box area.
-    // These can be set to empty gameobjects, which are placed throughout the level, called "right bounds" or etc.
-    public Transform upperVerticalDeadzone; 
-    public Transform lowerVerticalDeadzone;
-    public Transform rightHorizontalDeadzone;
-    public Transform leftHorizontalDeadzone;
+    public GameObject trackedObject; // used for parallax math
+    public Camera trackedCamera; // used for bounds
+    public Vector2 prevTrackedPosition;
 
-    // Move the background left as the player moves right
-    public void ScrollLeft()
+    // Set trackedcamera to main camera by default
+    private void Start()
     {
-
+        trackedCamera = Camera.main;
+        prevTrackedPosition = trackedCamera.transform.position;
     }
 
-    // Move the background right as the player moves left
-    public void ScrollRight()
+    private void Update()
     {
+        Vector2 newPos = trackedObject.transform.position;
+        Vector2 difference = newPos - prevTrackedPosition;
+        prevTrackedPosition = newPos;
 
+        // move background1 according to tracked object movement
+        var layers = background1.layers;
+        var yCoeff = background1.parallaxChangeValueY;
+        var xCoeff = background1.parallaxChangeValueX;
+        var origins = background1.originPositions;
+        var offsets = background1.offsets;
+
+        // Move each layer for BG 1
+        for (int i = 0; i < background1.layers.Length; i++)
+        {
+            // scroll layer
+            layers[i].transform.position = origins[i] + offsets[i] + new Vector3(prevTrackedPosition.x * xCoeff[i], prevTrackedPosition.y * yCoeff[i]);
+
+            if (difference.x > 0)
+            {
+                // move layer to the right if needed
+                ReplaceRight(difference.x, background1, i);
+            }
+            else if (difference.x < 0)
+            {
+                // move layer to left if needed
+                ReplaceLeft(difference.x, background1, i);
+            }
+        }
+
+        // Now, interact only with BG 2
+        layers = background2.layers;
+        yCoeff = background2.parallaxChangeValueY;
+        xCoeff = background2.parallaxChangeValueX;
+        origins = background2.originPositions;
+        offsets = background2.offsets;
+
+        for (int i = 0; i < background2.layers.Length; i++)
+        {
+            layers[i].transform.position = origins[i] + offsets[i] + new Vector3(prevTrackedPosition.x * xCoeff[i], prevTrackedPosition.y * yCoeff[i]);
+            if (difference.x > 0)
+            {
+                // move layer to the right if needed
+                ReplaceRight(difference.x, background2, i);
+            }
+            else if (difference.x < 0)
+            {
+                // move layer to the left if needed
+                ReplaceLeft(difference.x, background2, i);
+            }
+        }        
+        
     }
 
-    // Move the background up as the player moves up
-    public void ScrollUp()
+    // Move the rightmost background to the left side of the player as they move left.
+    private void ReplaceLeft(float difference, BackgroundPieceState background, int index)
     {
-
+        var layer = background.layers[index];
+        var sprite = layer.GetComponent<SpriteRenderer>();
+        if( layer.transform.position.x - trackedObject.transform.position.x > sprite.bounds.size.x)
+        {
+            //background.originPositions[index] -= Vector3.right * sprite.bounds.size.x*2;
+            background.offsets[index] -= Vector3.right * sprite.bounds.size.x * 2;
+        }
     }
 
-    //move the background down as the player moves down
-    public void ScrollDown()
+    // Move the leftmost background to the right side of the player as they move.
+    private void ReplaceRight(float difference, BackgroundPieceState background, int index)
     {
+        var layer = background.layers[index];
+        var sprite = layer.GetComponent<SpriteRenderer>();
+        if ( trackedObject.transform.position.x - layer.transform.position.x > sprite.bounds.size.x)
+        {
+            //background.originPositions[index] += Vector3.right * sprite.bounds.size.x*2;
+            background.offsets[index] += Vector3.right * sprite.bounds.size.x * 2;
+
+        }
 
     }
 
--- Magical Birds/Assets/Scripts/Level/BackgroundPieceState.cs
+++ Magical Birds/Assets/Scripts/Level/BackgroundPieceState.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections;
+﻿// Script made by Samuel Scherer
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundPieceController : MonoBehaviour
+public class BackgroundPieceState : MonoBehaviour
 {
     // This script simply keeps track of a background GameObjects layers.
     // For use with BackgroundParallax.cs
     public GameObject[] layers;
     public float[] parallaxChangeValueX;
     public float[] parallaxChangeValueY;
+    public Vector3[] offsets;
     public Vector3[] originPositions; // Is reset in start function
+    
 
     private void Start()
     {
         // Track the starting locations of each object. 
         originPositions = new Vector3[layers.Length];
+        offsets = new Vector3[layers.Length];
 
         for(int i = 0; i < layers.Length; i++)
         {
             originPositions[i] = layers[i].transform.position; // Save the starting location of each layer to this array
+            offsets[i] = Vector3.zero; // This will be updated in BackgroundParallax.cs
         }
     }
 
