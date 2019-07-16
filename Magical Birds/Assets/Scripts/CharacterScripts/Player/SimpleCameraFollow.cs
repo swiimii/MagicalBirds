7eaff4747e703b4c7b7e5e93786ea2814dfae337
Samuel Scherer <scheresr@mail.uc.edu>
2019-07-16 19:22:50 -0400

    Made camera changes.

--- Magical Birds/Assets/Scripts/CharacterScripts/Player/SimpleCameraFollow.cs
+++ Magical Birds/Assets/Scripts/CharacterScripts/Player/SimpleCameraFollow.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class SimpleCameraFollow : MonoBehaviour
 {
     public GameObject player;
-    public int cameraOffset;
+    public Vector3 cameraOffset;
     // Update is called once per frame
+    private void Start()
+    {
+        if(!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
     void Update()
     {
         if (player) // If player object isn't null, follow the player's position
         {
             var ptp = player.transform.position;
-            transform.position = new Vector3(ptp.x, ptp.y, cameraOffset);
+            transform.position = new Vector3(ptp.x, ptp.y) + cameraOffset;
         }
     }
 }
