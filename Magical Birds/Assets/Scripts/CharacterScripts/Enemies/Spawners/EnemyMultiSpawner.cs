64eb6322aa992a6d9c31927809c90354b325fdc3
Samuel Scherer <scheresr@mail.uc.edu>
2019-07-16 14:33:28 -0400

    Added multiple spawners, and made balance changes
    Spawners created. Single spawner, and multi spawner.
    
    Basic Attack scale changed, so attack is larger. Knockback reduced
    
    Changed z position for several things. Attacks > enemies > friendly birds > ground.

---
+++ Magical Birds/Assets/Scripts/CharacterScripts/Enemies/Spawners/EnemyMultiSpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMultiSpawner : EnemySpawner
+{
+    // enemyPrefab player distanceUntilSpawn enemy fixedDirection directionIfFixed
+    // all inherited from EnemySpawner
+
+    // These lists must be the same size
+    public List<Transform> spawnPositions;
+    public List<GameObject> enemies;
+    public bool repeats;
+    private bool expended = false;
+
+    private void Start()
+    {
+        if(!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        CreateEnemy();
+
+    }
+
+    protected override void Update()
+    {
+        
+        if (Mathf.Abs(transform.position.x - player.transform.position.x) < distanceUntilSpawn)
+        {
+
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                // Activate Enemy if player is within a distance threshold.
+                if (enemies[i] && !enemies[i].activeInHierarchy)
+                {
+                    enemies[i].SetActive(true);
+                }
+                
+            }
+
+            if (!repeats)
+            {
+                gameObject.GetComponent<EnemyMultiSpawner>().enabled = false;
+            }
+
+        }
+
+        else
+        {
+            for (int i = 0; i < spawnPositions.Count; i++)
+            {
+                if (!enemies[i])
+                {
+                    CreateEnemy(spawnPositions[i].position, i);
+                }
+            }
+
+        }
+        
+    }
+
+
+
+    public void CreateEnemy(Vector3 position, int index)
+    {
+        // Instantiate Enemy
+        enemies[index] = Instantiate(enemyPrefab, spawnPositions[index]);
+        enemies[index].transform.position = position;
+
+        // Make the enemy face the direction of the player
+        enemies[index].GetComponent<EnemyMovementController>().SetDirection(GetDirection());
+
+        //Only set active in update function
+        enemies[index].SetActive(false);
+    }
+
+}
